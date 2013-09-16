acc2f17234194a8c8e07881317e2fe95f58d4b03
Allan Farinas <awesomeplusplus@riseup.net>
2013-09-16 01:24:46 -0700

    Finished all program functionality, including printing triforces of negative sizes (which displays upside down)

--- triforce.py
+++ triforce.py
@@ -4,15 +4,14 @@ import sys
 
 def main():
 	n = int(sys.argv[1])
-	if n < 2:
-		print "Number needs to be more than 1!"
+	if n < 0:
+		n *= -1
+		print_negative_triforce(n)
 	else:
 		print_triforce(n)
-	#print("The number you entered is " + n);
 
 def print_triforce(n):
 	height = 2 * n
-	small_height = n
 	base = height - 1
 
 	# print the top triangle
@@ -34,6 +33,32 @@ def print_triforce(n):
 		for l in xrange(0, base - (2 * i)):
 			line += " "
 		for m in xrange(0, 1 + (2 * i)):
+			line += "*"
+		print(line)
+
+def print_negative_triforce(n):
+	height = 2 * n
+	base = height - 1
+
+	# print the top two trianagles
+	for i in xrange(0, n):
+		line = ""
+		for j in xrange(0, i):
+			line += " "
+		for k in xrange(0, base - (2 * i)):
+			line += "*"
+		for l in xrange(0, 1 + (2 * i)):
+			line += " "
+		for m in xrange(0, base - (2 * i)):
+			line += "*"
+		print(line)
+
+	# print the bottom triangle
+	for i in xrange (0, n):
+		line = ""
+		for j in xrange(0, n + i):
+			line += " "
+		for k in xrange(0, base - (2 * i)):
 			line += "*"
 		print(line)
 
